ffc9e64ef99ce981e2d6c0fe4f8b9384849fb5ea
SeHoon <sehoon1989@gmail.com>
2024-12-29 22:51:55 +0900

    feature/add-vector-chain

---
+++ learn-langchain/intro-vector-dbs/main.py
@@ -0,0 +1,43 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_pinecone import PineconeVectorStore
+
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+
+
+load_dotenv()
+
+
+if __name__ == "__main__":
+    print(" Retrieving...")
+
+    # 임베딩 및 LLM 모델 초기화
+    embeddings = OpenAIEmbeddings()
+    llm = ChatOpenAI(model_name="gpt-4o-mini")
+
+    query = "what is Pinecone in machine learning?"
+    chain = PromptTemplate.from_template(template=query) | llm
+    # result = chain.invoke(input={})
+    # print(result.content)
+
+    # 백터 스토어 초기화
+    vectorstore = PineconeVectorStore(
+        # env에 선언된 INDEX_NAME을 사용하여 백터 스토어 초기화
+        index_name=os.environ["INDEX_NAME"], embedding=embeddings
+    )
+
+    # 질의 응답 체인 생성
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
+    retrival_chain = create_retrieval_chain(
+        retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain
+    )
+
+    result = retrival_chain.invoke(input={"input": query})
+
+    print(result)
