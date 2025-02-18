701ff26040355d8576f8fad862de44fbb424a4b7
SeHoon <sehoon1989@gmail.com>
2025-02-19 00:28:56 +0900

    feature/fix-conversation-url

--- learn-langchain/chatbot/backend/mysite/urls.py
+++ learn-langchain/chatbot/backend/mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("chatbot/", include("chatbot.urls")),
+    path("api/chatbot/", include("chatbot.urls")),
     path("admin/", admin.site.urls),
 ]
