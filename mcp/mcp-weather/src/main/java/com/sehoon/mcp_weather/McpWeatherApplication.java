90fa60ad641d246ffdeb1d25b99b1ece7222c6d5
sehoon <sehoon1989@gmail.com>
2025-05-01 22:17:55 +0900

    feature/init-mcp-weather

---
+++ mcp/mcp-weather/src/main/java/com/sehoon/mcp_weather/McpWeatherApplication.java
@@ -0,0 +1,22 @@
+package com.sehoon.mcp_weather;
+
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.sehoon.mcp_weather.module.weather.service.WeatherService;
+
+@SpringBootApplication
+public class McpWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpWeatherApplication.class, args);
+	}
+	
+	@Bean
+	public ToolCallbackProvider weatherTools(WeatherService weatherService) {
+		return  MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
+	}
+}
---
+++ mcp/mcp-weather/src/main/java/com/sehoon/mcp_weather/module/weather/service/WeatherService.java
@@ -0,0 +1,47 @@
+package com.sehoon.mcp_weather.module.weather.service;
+
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class WeatherService {
+
+	private final RestClient restClient;
+
+	public WeatherService() {
+		this.restClient = RestClient.builder()
+			.baseUrl("https://api.weather.gov")
+			.defaultHeader("Accept", "application/geo+json")
+			.defaultHeader("User-Agent", "WeatherApiClient/1.0 (your@email.com)")
+			.build();
+	}
+
+  @Tool(description = "Get weather forecast for a specific latitude/longitude")
+  public String getWeatherForecastByLocation(
+      double latitude,   // Latitude coordinate
+      double longitude   // Longitude coordinate
+  ) {
+      // Returns detailed forecast including:
+      // - Temperature and unit
+      // - Wind speed and direction
+      // - Detailed forecast description
+      return "";
+  }
+	
+  @Tool(description = "Get weather alerts for a US state")
+  public String getAlerts(
+      @ToolParam(description = "Two-letter US state code (e.g. CA, NY)") String state
+  ) {
+      // Returns active alerts including:
+      // - Event type
+      // - Affected area
+      // - Severity
+      // - Description
+      // - Safety instructions
+      return "";
+  }
+
+  // ......
+}
