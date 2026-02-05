7f4661d32f12a491c6b1a220ac3c658b31a0628f
sehoon <sehoon1989@gmail.com>
2026-02-05 22:27:56 +0900

    feat: add-docker-compose

--- admin/llm-admin/next.config.mjs
+++ admin/llm-admin/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   // If you are using Clerk, you might need to add this
   // images: {
